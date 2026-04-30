9601c1aa9c92599a2ae95b91aa4cbb554f14f510
LarahDejan <dejanlarah1@gmail.com>
2026-04-30 21:34:21 +0800

    Upload backend files

---
+++ API/submit.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();    
+
+router.post('/', (req, res) => {
+    const { name, age, email, talent } = req.body;
+
+    console.log("Received Data: ", {name, age, email, talent})
+
+    res.status(200).json({ message: 'Form Submitted Successfully' });
+
+});
